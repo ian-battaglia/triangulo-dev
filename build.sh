9034a09afc3e0c1ebf925809b385bd27ea5c823c
Hugo Bessa <eu@hugobessa.com.br>
2019-06-04 15:00:35 -0300

    fix: Brute force baseURL

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 curl -L -O https://github.com/gohugoio/hugo/releases/download/v0.55.6/hugo_0.55.6_Linux-64bit.tar.gz
 tar -xzf hugo_0.55.6_Linux-64bit.tar.gz
 
-./hugo
+mkdir -p ./tmp
+echo "baseURL = \"https://triangulo.dev/\"" >> ./tmp/config.toml
+
+./hugo --config config.toml,./tmp/config.toml
